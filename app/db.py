821c3a95ded565c5a9c28d0aab840fa92002cacf
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-02-14 23:40:00 -0500

    Refactor db connection

--- app/db.py
+++ app/db.py
@@ -1,88 +1,48 @@
 import asyncio
 import asyncpg
-from .config.app import (
-    DB_SERVICES_HOST,
-    DB_SERVICES_NAME,
-    DB_SERVICES_PASSWORD,
-    DB_SERVICES_USER,
-    DB_WEBHOOKS_HOST,
-    DB_WEBHOOKS_NAME,
-    DB_WEBHOOKS_PASSWORD,
-    DB_WEBHOOKS_USER
-)
+from asyncpg.exceptions import PostgresError
 
-async def start_webhooks_db_connection(app):
+
+class Database:
     """
-    Creates a connection to webhooks database
-    Args:
-        app: fastapi instance
+    Database connection handler, it manage the database connection and handles the errors
     """
-    app.state.webhooks_db = await asyncpg.create_pool(
-        dsn=("postgresql://{user}:{password}@{host}/{name}"
-            .format(
-                user=DB_WEBHOOKS_USER,
-                password=DB_WEBHOOKS_PASSWORD,
-                host=DB_WEBHOOKS_HOST,
-                name=DB_WEBHOOKS_NAME
-            )
-        )
-    )
-    print("[x] Webhooks database connected ... OK")
     
-async def start_services_db_connection(app):
-    """
-    Creates a connection to services database
-    Args:
-        app: fastapi instance
-    """
-    app.state.services_db = await asyncpg.create_pool(
-        dsn=("postgresql://{user}:{password}@{host}/{name}"
-            .format(
-                user=DB_SERVICES_USER,
-                password=DB_SERVICES_PASSWORD,
-                host=DB_SERVICES_HOST,
-                name=DB_SERVICES_NAME
-            )
-        )
-    )
-    print("[x] Services database connected ... OK")
-    
-async def check_webhooks_connection(app):
-    """
-    Validates if database connection exists, if so, it makes a query to database to ensure the
-    connection is working correctly, if not, it calls to the connection logic to restablish
-    the db connection
-    Args:
-        app: fastapi instance
-    """
-    while True:
+    def __init__(self, host: str, name: str, user: str, password: str, port: str) -> None:
+        self._name: str = name
+        self.dsn=(f"postgresql://{user}:{password}@{host}:{port}/{name}")
+        self.conn = None
+
+    async def connect(self) -> None:
+        """
+        Creates db connection
+        """
         try:
-            async with app.state.webhooks_db.acquire() as connection:
-                await connection.fetch("SELECT 1")
-        except (
-            asyncpg.exceptions.ConnectionDoesNotExistError, 
-            asyncpg.exceptions.ConnectionFailureError
-        ):
-            print("[x] Failed to connect to webhooks database, retrying...")
-            await start_webhooks_db_connection()
-        await asyncio.sleep(60)
-        
-async def check_services_connection(app):
-    """
-    Validates if database connection exists, if so, it makes a query to database to ensure the
-    connection is working correctly, if not, it calls to the connection logic to restablish
-    the db connection
-    Args:
-        app: fastapi instance
-    """
-    while True:
+            self.conn = await asyncpg.connect(self.dsn)
+            print(f"[x] {self._name.capitalize()} database connection ... OK")
+        except PostgresError as e:
+            print(f"[x] Could not connect to database: {e}")
+            await asyncio.sleep(15)
+            await self.connect()
+
+    async def disconnect(self) -> None:
+        """
+        Closes db connection
+        """
+        await self.conn.close()
+
+    async def execute(self, query: str, *args):
+        """
+        Executes queries
+        Args:
+            query
+        """
+        if not self.conn:
+            await self.connect()
         try:
-            async with app.state.services_db.acquire() as connection:
-                await connection.fetch("SELECT 1")
-        except (
-            asyncpg.exceptions.ConnectionDoesNotExistError, 
-            asyncpg.exceptions.ConnectionFailureError
-        ):
-            print("[x] Failed to connect to services database, retrying...")
-            await start_services_db_connection()
-        await asyncio.sleep(60)
+            return await self.conn.execute(query, *args)
+        except PostgresError as e:
+            print(f"[x] Error executing query: {e}")
+            await self.disconnect()
+            await asyncio.sleep(5)
+            return await self.execute(query, *args)
