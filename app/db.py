3a76adfc441ffbf32687d06331419f226333607c
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-06 22:34:49 -0500

    Fix json encoding response

--- app/db.py
+++ app/db.py
@@ -1,24 +1,35 @@
+import json
 import asyncio
-import asyncpg
+from asyncpg import connect, Connection
 from asyncpg.exceptions import PostgresError
-
+from typing import Optional, List
 
 class Database:
     """
     Database connection handler, it manage the database connection and handles the errors
     """
     
+    _name: str
+    _dsn: str
+    _conn: Optional[Connection]
+    
     def __init__(self, host: str, name: str, user: str, password: str, port: str) -> None:
         self._name: str = name
-        self.dsn=(f"postgresql://{user}:{password}@{host}:{port}/{name}")
-        self.conn = None
+        self._dsn = f"postgresql://{user}:{password}@{host}:{port}/{name}"
+        self._conn = None
 
     async def connect(self) -> None:
         """
         Creates db connection
         """
         try:
-            self.conn = await asyncpg.connect(self.dsn)
+            self._conn = await connect(self._dsn)
+            await self._conn.set_type_codec(
+                'json',
+                encoder=json.dumps,
+                decoder=json.loads,
+                schema='pg_catalog'
+            )
             print(f"[x] {self._name.capitalize()} database connection ... OK")
         except PostgresError as e:
             print(f"[x] Could not connect to database: {e}")
@@ -29,18 +40,18 @@ class Database:
         """
         Closes db connection
         """
-        await self.conn.close()
+        await self._conn.close()
 
-    async def execute(self, query: str, *args):
+    async def execute(self, query: str, *args: Optional[List]):
         """
         Executes queries
         Args:
             query
         """
-        if not self.conn:
+        if not self._conn:
             await self.connect()
         try:
-            return await self.conn.execute(query, *args)
+            return await self._conn.fetch(query, *args)
         except PostgresError as e:
             print(f"[x] Error executing query: {e}")
             await self.disconnect()
