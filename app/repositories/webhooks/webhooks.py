527a25d292ba2b21478e9e19fe475bb19d67f45c
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-09 23:50:09 -0500

    Import app instance inside webhooks repository

--- app/repositories/webhooks/webhooks.py
+++ app/repositories/webhooks/webhooks.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
 
-from app.main import app
+from app.app import app
 
 async def get_all_webhooks() -> List[Dict]:
     query: str = f"""
@@ -21,7 +21,7 @@ async def get_all_webhooks() -> List[Dict]:
     data = await app.state.service_db.execute(query)
     return data
 
-async def get_webhook(user_id: int, event_pattern: str) -> List[Dict]:
+async def get_webhook_by_id(user_id: int = None, event_pattern: str = None) -> List[Dict]:
     query: str = f"""
     SELECT
         w.id,
