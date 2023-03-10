cf112903f68f0bf2be1976f84a742fbb7df05b90
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-09 23:48:41 -0500

    Changed db instance creation inside starup event and closed rabbit connection when shutdown event

--- app/main.py
+++ app/main.py
@@ -1,26 +1,22 @@
-from fastapi import FastAPI
-
 import asyncio
 
 from .amqp import start_amqp_listener
 from .config.app import WEBHOOKS_DB, SERVICES_DB
 from .db import Database
-
-app = FastAPI()
-
-webhook_db = Database(**WEBHOOKS_DB)
-service_db = Database(**SERVICES_DB)
+from .app import app
 
 @app.on_event("startup")
 async def startup_event() -> None:
     """
     Initialices databases and RabbitMQ listener when service starts
     """
+    webhook_db = Database(**WEBHOOKS_DB)
+    service_db = Database(**SERVICES_DB)
     await service_db.connect()
     app.state.service_db = service_db
     await webhook_db.connect()
     app.state.webhook_db = webhook_db
-    asyncio.create_task(start_amqp_listener(app))
+    asyncio.create_task(start_amqp_listener())
 
 @app.on_event("shutdown")
 async def shutdown() -> None:
@@ -29,6 +25,7 @@ async def shutdown() -> None:
     """
     await app.state.webhooks_db.disconnect()
     await app.state.services_db.disconnect()
+    await app.state.rabbit.close()
 
 @app.get("/")
 async def read_root():
